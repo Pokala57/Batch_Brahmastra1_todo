6c7e8476ff51547dcb1ff59f4cbdfb9647f3ceed
Pokala57 <sathish@vridhee.com>
2024-06-18 21:09:03 +0530

    after some edit

--- index.js
+++ index.js
@@ -5,25 +5,144 @@ let userInputButton = document.getElementById("userAddButton")
 let userTaskListContainer = document.getElementById("todoUList")
 
 
+let todoList = [
+  {
+    text: "Learn HTML",
+    uniqueNo: 1
+  },
+  {
+    text: "Learn CSS",
+    uniqueNo: 2
+  },
+  {
+    text: "Learn JavaScript",
+    uniqueNo: 3
+  }
+];
 
-function createAndAppendList(userEnterdData){
-
-    let listElement =  document.createElement("li")
-
-    listElement.textContent = userEnterdData
+let todoListLenght = todoList.length
 
 
-    userTaskListContainer.appendChild(listElement)
+function oncompletedTodo(checkBoxId ,lableId){
+
+  let checkBox = document.getElementById(checkBoxId)
+
+  let lableElemnt = document.getElementById(lableId)
+
+  lableElemnt.classList.toggle("checked")
+
+}
 
 
 
+function createAndAppendList(userEnterdData) {
+
+  let todoId = `todo` + userEnterdData.uniqueNo
+
+  let checkBoxId = `checkbox` + userEnterdData.uniqueNo
+
+  let lableId = `label` + userEnterdData.uniqueNo
+
+
+  let listElement = document.createElement("li")
+
+  listElement.classList.add("todo-item-container")
+
+  listElement.id = todoId
+
+  // input Element create
+
+
+  let inputElement = document.createElement("input")
+
+  inputElement.type = "checkbox"
+
+  inputElement.id = checkBoxId
+
+  inputElement.onclick = function () {
+
+    oncompletedTodo(checkBoxId,lableId)
+
+  }
+
+  inputElement.classList.add("checkbox-input")
+
+
+
+  listElement.appendChild(inputElement)
+
+
+  // create lable element
+
+
+  let lableContainer = document.createElement("div")
+
+  lableContainer.classList.add("label-container")
+
+
+  let lableElemnt = document.createElement("label")
+
+  lableElemnt.setAttribute("for", checkBoxId)
+
+
+  lableElemnt.id = lableId
+
+  lableElemnt.classList.add("checkbox-label")
+
+  lableElemnt.textContent = userEnterdData.text
+
+  lableContainer.appendChild(lableElemnt)
+
+
+  // create delete container
+
+
+  let deletContainer = document.createElement("div")
+
+  deletContainer.classList.add("delete-icon-container")
+
+
+  lableContainer.appendChild(deletContainer)
+
+
+  let deleteIcon = document.createElement("span")
+
+  deleteIcon.classList.add("material-symbols-outlined")
+
+  deleteIcon.textContent = "delete"
+
+
+  deleteIcon.onclick = function () {
+
+  }
+
+
+
+  deletContainer.appendChild(deleteIcon)
+
+
+  listElement.appendChild(lableContainer)
+
+
+  // listElement.textContent = userEnterdData
+
+
+  userTaskListContainer.appendChild(listElement)
+
+
+}
+
+
+for (let todoItem of todoList) {
+
+  createAndAppendList(todoItem)
 
 }
 
 
 userInputButton.onclick = function () {
 
-    createAndAppendList(userInputValue.value)
+  createAndAppendList(userInputValue.value)
 
 }
 
